6eacf5f387cbc19c0454fce2e38e438193c8809a
Ralf Biedert <rb@xr.io>
2021-11-22 18:23:57 +0100

    HOW CAN THIS STILL BE BROKEN?

--- core/src/error.rs
+++ core/src/error.rs
@@ -18,37 +18,3 @@ pub enum Error {
     /// A specified file was not found.
     FileNotFound,
 }
-
-impl From<std::fmt::Error> for Error {
-    fn from(e: std::fmt::Error) -> Self {
-        Self::Format(e)
-    }
-}
-
-impl From<std::io::Error> for Error {
-    fn from(e: std::io::Error) -> Self {
-        Self::IO(e)
-    }
-}
-
-impl From<std::str::Utf8Error> for Error {
-    fn from(e: std::str::Utf8Error) -> Self {
-        Self::UTF8(e)
-    }
-}
-
-impl From<std::string::FromUtf8Error> for Error {
-    fn from(e: std::string::FromUtf8Error) -> Self {
-        Self::FromUtf8(e)
-    }
-}
-
-impl Display for Error {
-    // TODO: This should be nicer.
-    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
-        write!(f, "Interoptopus failed!")
-    }
-}
-
-// TODO
-impl std::error::Error for Error {}
